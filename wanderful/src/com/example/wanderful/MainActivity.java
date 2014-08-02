733cf5200cd4a5379ac073e0cdd16accd29d7359
Darren Chua <darren.beats@gmail.com>
2014-08-02 20:15:49 +0800

    Implemented google maps

--- wanderful/src/com/example/wanderful/MainActivity.java
+++ wanderful/src/com/example/wanderful/MainActivity.java
@@ -1,15 +1,151 @@
 package com.example.wanderful;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import android.support.v4.app.FragmentActivity;
+
 import android.app.Activity;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Toast;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
 
-public class MainActivity extends Activity {
+import android.support.v4.app.Fragment;
+import android.util.Log;
 
+public class MainActivity extends FragmentActivity implements LocationListener {
+	
+	private GoogleMap googleMap;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		try {
+            // Loading map
+            initilizeMap();
+ 
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+	}
+	
+	private void initilizeMap() {
+        if (googleMap == null) {
+            // check if map is created successfully or not
+        	googleMap.setMyLocationEnabled(true); // false to disable
+        	googleMap.getUiSettings().setMyLocationButtonEnabled(true);
+        	googleMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+            if (googleMap == null) {
+                Toast.makeText(getApplicationContext(),
+                        "Sorry! unable to create maps", Toast.LENGTH_SHORT)
+                        .show();
+            }
+            getLocation();
+        }
+    }
+	
+	LocationManager locationManager;
+	boolean isGPSEnabled,isNetworkEnabled,canGetLocation;
+	// The minimum distance to change Updates in meters
+    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 0; // 10 meters
+ 
+    // The minimum time between updates in milliseconds
+    private static final long MIN_TIME_BW_UPDATES = 0; // 1 minute
+    double latitude,longitude;
+	public Location getLocation() {
+		Log.v("here","here");
+		Location location = null;
+        try {
+            locationManager = (LocationManager)getSystemService(LOCATION_SERVICE);
+ 
+            // getting GPS status
+            isGPSEnabled = locationManager
+                    .isProviderEnabled(LocationManager.GPS_PROVIDER);
+ 
+            // getting network status
+            isNetworkEnabled = locationManager
+                    .isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+ 
+            if (!isGPSEnabled && !isNetworkEnabled) {
+                // no network provider is enabled
+            } else {
+                this.canGetLocation = true;
+                // First get location from Network Provider
+                if (isNetworkEnabled) {
+                    locationManager.requestLocationUpdates(
+                            LocationManager.NETWORK_PROVIDER,
+                            MIN_TIME_BW_UPDATES,
+                            MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+                    Log.v("Network", "Network");
+                    if (locationManager != null) {
+                        location = locationManager
+                                .getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                        if (location != null) {
+                            latitude = location.getLatitude();
+                            longitude = location.getLongitude();
+                        }
+                    }
+                }
+                // if GPS Enabled get lat/long using GPS Services
+                if (isGPSEnabled) {
+                    if (location == null) {
+                        locationManager.requestLocationUpdates(
+                                LocationManager.GPS_PROVIDER,
+                                MIN_TIME_BW_UPDATES,
+                                MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+                        Log.d("GPS Enabled", "GPS Enabled");
+                        if (locationManager != null) {
+                            location = locationManager
+                                    .getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                            if (location != null) {
+                                latitude = location.getLatitude();
+                                longitude = location.getLongitude();
+                                Log.v("title",latitude+", "+longitude);
+                                CameraPosition cameraPosition = new CameraPosition.Builder().target(
+                                        new LatLng(location.getLatitude(), location.getLongitude())).zoom(12).build();
+                         
+                        googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+                            }
+                        }
+                    }
+                }
+            }
+ 
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+ 
+        return location;
+    }
+	
+	public void onLocationChanged(Location location) {
+		Log.v("here","here");
+		CameraPosition cameraPosition = new CameraPosition.Builder().target(
+                new LatLng(location.getLatitude(), location.getLongitude())).zoom(12).build();
+ 
+googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+    }
+
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onProviderEnabled(String provider) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onProviderDisabled(String provider) {
+		// TODO Auto-generated method stub
+		
 	}
 }
